2823a7cd5687e081b5e0fe1349534e2ebf3c4a9b
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2017-04-11 08:46:59 -0300

    Agregada clase User

---
+++ src/main/java/models/User.java
@@ -0,0 +1,25 @@
+package models;
+
+public class User {
+	private String name;
+	private String password;
+	private String admission_date;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAdmission_date() {
+		return admission_date;
+	}
+	public void setAdmission_date(String admission_date) {
+		this.admission_date = admission_date;
+	}
+}
